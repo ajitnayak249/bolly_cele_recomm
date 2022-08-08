3917bf6f0ef5cb44f888278288a6dc8a36dd1ec3
Ajit Nayak <64416485+ajitnayak249@users.noreply.github.com>
2022-08-09 00:26:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,93 @@
+from keras_vggface.utils import preprocess_input
+from keras_vggface.vggface import VGGFace
+import numpy as np
+import pickle
+from sklearn.metrics.pairwise import cosine_similarity
+import streamlit as st
+from PIL import Image
+import os
+import cv2
+from mtcnn import MTCNN
+
+detector = MTCNN()
+
+model = VGGFace(model='resnet50', include_top=False, input_shape=(224,224,3), pooling='avg')
+
+feature_list = pickle.load(open('embedding.pkl', 'rb'))
+
+filenames = pickle.load(open('filenames.pkl', 'rb'))
+
+def save_uploaded_image(uploaded_image):
+    try:
+        with open(os.path.join('uploads', uploaded_image.name), 'wb') as f:
+            f.write(uploaded_image.getbuffer())
+        return True
+    except:
+        return False
+
+def extract_features(img_path, model, detector):
+    img = cv2.imread(img_path)
+    results = detector.detect_faces(img)
+
+    x, y, width, height = results[0]['box']
+    face = img[y:y + height, x:x + width]
+
+    # cv2.imshow('output', face)
+    # cv2.waitKey(0)
+
+    # extraxt its features
+    image = Image.fromarray(face)
+    image = image.resize((224, 224))
+
+    face_array = np.asarray(image)
+
+    face_array = face_array.astype('float32')
+
+    expended_img = np.expand_dims(face_array, axis=0)
+
+    preprocessed_img = preprocess_input((expended_img))
+
+    result = model.predict(preprocessed_img).flatten()
+
+    return result
+
+def recommend(feature_list, features):
+    similarity = []
+
+    for i in range(len(feature_list)):
+        similarity.append(cosine_similarity(features.reshape(1, -1), feature_list[i].reshape(1, -1))[0][0])
+
+    # print(len(similarity))
+    index_pos = sorted(list(enumerate(similarity)), reverse=True, key=lambda x: x[1])[0][0]
+    return index_pos
+
+st.title("Which Bollywood Celebrity are you?")
+
+uploaded_image = st.file_uploader('Choose a Image')
+
+if uploaded_image is not None:
+    # save the image in a directort
+    if save_uploaded_image(uploaded_image):
+
+        #load the image
+        display_image = Image.open(uploaded_image)
+        #st.image(display_image)
+
+        #extract the features
+        features = extract_features(os.path.join('uploads', uploaded_image.name), model, detector)
+        # st.text(features)
+        # st.text(features.shape)
+        #recommend
+        index_pos = recommend(feature_list,features)
+
+        predicted_actor = " ".join(filenames[index_pos].split('\\')[1].split('_'))
+        #display
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.header('Your Uploaded Image')
+            st.image(display_image)
+
+        with col2:
+            st.header("Seems Like " + predicted_actor)
+            st.image(filenames[index_pos], width = 300)
